93125a01ba4f11413128159fa8f1fb937765d9b4
Pratikshakadam0712 <126559649+Pratikshakadam0712@users.noreply.github.com>
2023-03-07 16:48:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 
-cd snowflake-connector-python
+pip install -r https://raw.githubusercontent.com/snowflakedb/snowflake-connector-python/v3.0.0/tested_requirements/requirements_36.reqs
 
 
 import streamlit
